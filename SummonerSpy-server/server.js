30d6e9be633eae61180e96aab4195d21b02da892
marinactonci <toncimarinac@gmail.com>
2023-07-28 03:13:36 +0200

    Final

--- SummonerSpy-server/server.js
+++ SummonerSpy-server/server.js
@@ -3,66 +3,285 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
 const cors = require('cors');
+const firebase = require('firebase');
 const port = 4320;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-axios.defaults.headers.common['X-Riot-Token'] = 'RGAPI-af8999fe-31fe-45e2-b0f2-6a19c2948cfb';
+axios.defaults.headers.common['X-Riot-Token'] = 'RGAPI-367807b7-7670-46db-adf9-179f90be46b1';
 
-app.get('summoner/:region/:summonerName', (req, res) => {
+const firebaseConfig = {
+    apiKey: "AIzaSyDjdorNRCh8pjbJNVRkkfBIA052ojYYwts",
+    authDomain: "summonerspy-ad89d.firebaseapp.com",
+    projectId: "summonerspy-ad89d",
+    storageBucket: "summonerspy-ad89d.appspot.com",
+    messagingSenderId: "662417332725",
+    appId: "1:662417332725:web:1845f29683ef224469a8bc"
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const profileAccountsCollection = firebase.firestore().collection('profileAccounts');
+const favoritesCollection = firebase.firestore().collection('favorites');
+
+app.get('/profile-account/:userId', async(req, res) => {
+    const { userId } = req.params;
+    try {
+        const snapshot = await profileAccountsCollection.doc(userId).get();
+
+        if (!snapshot.exists) {
+            return res.json({ message: 'Document not found' });
+        }
+
+        const documentData = snapshot.data();
+        return res.status(200).json(documentData);
+    } catch (error) {
+        return res.json({ error: error });
+    }
+});
+
+app.post('/add-profile-account/:userId/:region/:name', async(req, res) => {
+    const { userId, region, name } = req.params;
+    try {
+        const snapshot = await profileAccountsCollection.doc(userId).get();
+
+        if (!snapshot.exists) {
+            await profileAccountsCollection.doc(userId).set({
+                region: region,
+                name: name
+            });
+
+            const documentData = snapshot.data();
+            return res.status(200).json(documentData);
+        }
+
+        const documentData = snapshot.data();
+        const profileAccount = {
+            region: region,
+            name: name
+        };
+
+        await profileAccountsCollection.doc(userId).update(profileAccount);
+
+        return res.status(200).json({ message: 'Profile account updated' });
+    } catch (error) {
+        console.error('Error getting document:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.delete('/remove-profile-account/:userId', async(req, res) => {
+    const { userId } = req.params;
+    try {
+        const snapshot = await profileAccountsCollection.doc(userId).get();
+
+        if (!snapshot.exists) {
+            return res.status(404).json({ message: 'Document not found' });
+        }
+
+        await profileAccountsCollection.doc(userId).delete();
+        return res.status(200).json({ message: 'Profile account deleted' });
+    } catch (error) {
+        console.error('Error getting document:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.get('/get-favorites/:userId', async(req, res) => {
+    const { userId } = req.params;
+    try {
+        const snapshot = await favoritesCollection.doc(userId).get();
+
+        if (!snapshot.exists) {
+            await favoritesCollection.doc(userId).set({
+                favorites: []
+            });
+            return res.status(200).json({ message: 'Document created' });
+        }
+
+        const documentData = snapshot.data();
+        return res.status(200).json(documentData);
+    } catch (error) {
+        return res.status(500).json({ error: error });
+    }
+});
+
+app.post('/add-favorites/:userId/:region/:name', async(req, res) => {
+    const { userId, region, name } = req.params;
+    try {
+        const snapshot = await favoritesCollection.doc(userId).get();
+
+        if (!snapshot.exists) {
+            await favoritesCollection.doc(userId).set({
+                favorites: [{
+                    region: region,
+                    name: name
+                }]
+            });
+            return res.status(200).json({ message: 'Document created' });
+        }
+
+        const documentData = snapshot.data();
+        const favorites = documentData.favorites;
+
+        favorites.push({
+            region: region,
+            name: name
+        });
+
+        await favoritesCollection.doc(userId).update({
+            favorites: favorites
+        });
+
+        return res.status(200).json({ message: 'Favorite added' });
+    } catch (error) {
+        console.error('Error getting document:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.delete('/delete-favorites/:userId/:region/:name', async(req, res) => {
+    const { userId, region, name } = req.params;
+    try {
+        const snapshot = await favoritesCollection.doc(userId).get();
+
+        if (!snapshot.exists) {
+            return res.status(404).json({ message: 'Document not found' });
+        }
+
+        const documentData = snapshot.data();
+        let favorites = documentData.favorites;
+
+        favorites = favorites.filter(favorite => favorite.name !== name || favorite.region !== region);
+
+        await favoritesCollection.doc(userId).update({
+            favorites: favorites
+        });
+
+        return res.status(200).json({ message: 'Favorite removed' });
+    } catch (error) {
+        console.error('Error getting document:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.post('/register/:email/:password', async(req, res) => {
+    const {email, password} = req.params;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            res.status(200).send(userCredential);
+        })
+        .catch((error) => {
+            res.status(404).send(error);
+        });
+});
+
+app.post('/login/:email/:password', async(req, res) => {
+    const {email, password} = req.params;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            res.status(200).send(userCredential);
+        })
+        .catch((error) => {
+            res.status(404).send(error);
+        });
+});
+
+app.get('/logout', function(req , res){
+    firebase.auth().signOut().then(() => {
+        res.send({message: "Logged out!"});
+    }).catch((error) => {
+        res.send({message: "Error!", error: error});
+    });
+});
+
+app.post('/change-password/:password/:newPassword', async(req, res) => {
+    const {password, newPassword} = req.params;
+    const user = firebase.auth().currentUser;
+    const credential = firebase.auth.EmailAuthProvider.credential(
+        user.email,
+        password
+    );
+    user.reauthenticateWithCredential(credential).then(() => {
+        user.updatePassword(newPassword).then(() => {
+            res.send({message: "Password changed!"});
+        }).catch((error) => {
+            res.send({message: "Error!", error: error});
+        });
+    }). catch((error) => {
+        res.send({message: "Error!", error: error});
+    });
+});
+
+app.get('/user-status', (req, res) => {
+    res.send(firebase.auth().currentUser);
+})
+
+app.get('/summoner/:region/:summonerName', (req, res) => {
     const { region, summonerName } = req.params;
     axios.get(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`)
         .then(response => {
             res.status(200).send(response.data);
         })
         .catch(error => {
-            console.error('Error getting summoner', error);
+            console.log(error);
+            res.send({});
         });
 });
 
-app.get('rankPoints/:region/:id', (req, res) => {
+app.get('/rankPoints/:region/:id', (req, res) => {
     const { region, id } = req.params;
     axios.get(`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}`)
         .then(response => {
             res.status(200).send(response.data);
         })
         .catch(error => {
-            console.error('Error getting rank points', error);
+            res.status(404).send(error.response.data);
         });
 });
 
-app.get('masteryPoints/:region/:id', (req, res) => {
+app.get('/masteryPoints/:region/:id', (req, res) => {
     const { region, id } = req.params;
-    axios.get(`https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}`)
+    axios.get(`https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}/top`)
         .then(response => {
             res.status(200).send(response.data);
         })
         .catch(error => {
-            console.error('Error getting rank points', error);
+            res.status(404).send(error.response.data);
         });
 });
 
-app.get('allMatches/:region/:puuid/:start/:count', (req, res) => {
+app.get('/allMatches/:region/:puuid/:start/:count', (req, res) => {
     const { region, puuid, start, count } = req.params;
     axios.get(`https://${region}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=${start}&count=${count}`)
         .then(response => {
             res.status(200).send(response.data);
         })
         .catch(error => {
-            console.error('Error getting rank points', error);
+            res.status(404).send(error.response.data);
         });
 });
 
-app.get('/:server/leaderboards', (req, res) => {
-    const { server } = req.params;
-    const queueType = 'RANKED_SOLO_5x5';
+app.get('/match/:region/:matchId', (req, res) => {
+    const { region, matchId } = req.params;
+    axios.get(`https://${region}.api.riotgames.com/lol/match/v5/matches/${matchId}`)
+        .then(response => {
+            res.status(200).send(response.data);
+        })
+        .catch(error => {
+            res.status(404).send(error.response.data);
+        });
+});
+
+app.get('/leaderboards/:server/:queueType', (req, res) => {
+    const { server, queueType } = req.params;
     axios.get(`https://${server}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/${queueType}`)
         .then(response => {
             res.status(200).send(response.data.entries);
         })
         .catch(error => {
-            console.error('Error getting leaderboards', error);
+            res.status(404).send(error.response.data);
         });
 });
 
@@ -72,7 +291,7 @@ app.get('/free-rotation', (req, res) => {
             res.status(200).send(response.data);
         })
         .catch(error => {
-            console.error('Error getting free rotation', error);
+            res.status(404).send(error.response.data);
         });
 });
 
